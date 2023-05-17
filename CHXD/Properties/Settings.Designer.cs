2b8a0f9c7e7a94402dfa8ed6b6b10ec30307b576
sonlh02 <let02hb@gmail.com>
2023-05-17 15:33:52 +0700

    commitNhaCungCap

--- CHXD/Properties/Settings.Designer.cs
+++ CHXD/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CHXD.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace CHXD.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Quanlybanhang" +
-            ".mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Project\\CHXD\\CHXD\\Quanlyba" +
+            "nhang.mdf;Integrated Security=True;Connect Timeout=30")]
         public string CHXDConnectionString {
             get {
                 return ((string)(this["CHXDConnectionString"]));
